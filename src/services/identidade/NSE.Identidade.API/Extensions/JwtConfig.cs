c5388b6656a6acad84d836e63a9b1a49efa822a3
Paulo Sampaio <paulohenrique.sales47@gmail.com>
2021-11-28 18:15:30 -0300

    Configurando o JWT na API

---
+++ src/services/identidade/NSE.Identidade.API/Extensions/JwtConfig.cs
@@ -0,0 +1,10 @@
+﻿namespace NSE.Identidade.API.Extensions
+{
+    public class JwtConfig
+    {
+        public string Audience { get; set; }
+        public string Issuer { get; set; }
+        public string key { get; set; }
+        public int ExpirationTimeHours { get; set; }
+    }
+}
--- src/services/identidade/NSE.Identidade.API/Program.cs
+++ src/services/identidade/NSE.Identidade.API/Program.cs
@@ -1,7 +1,11 @@
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
 using Microsoft.OpenApi.Models;
 using NSE.Identidade.API.Data;
+using NSE.Identidade.API.Extensions;
+using System.Text;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -13,17 +17,46 @@ builder.Services.AddDefaultIdentity<IdentityUser>()
     .AddEntityFrameworkStores<ApplicationDbContext>()
     .AddDefaultTokenProviders();
 
+
+var jwtSection = builder.Configuration.GetSection(nameof(JwtConfig));
+builder.Services.Configure<JwtConfig>(jwtSection);
+
+builder.Services.AddAuthentication(options =>
+{
+    options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+    options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+}).AddJwtBearer(bearerOptions =>
+{
+    var jwtConfig = jwtSection.Get<JwtConfig>();
+
+    bearerOptions.RequireHttpsMetadata = true;
+    bearerOptions.SaveToken = true;
+    bearerOptions.TokenValidationParameters = new TokenValidationParameters
+    {
+        ValidateIssuerSigningKey = true,
+        IssuerSigningKey = new SymmetricSecurityKey(Encoding.ASCII.GetBytes(jwtConfig.key)),
+        ValidateIssuer = true,
+        ValidateAudience = true,
+        ValidAudience = jwtConfig.Audience,
+        ValidIssuer = jwtConfig.Issuer,
+    };
+});
+
+
+
 builder.Services.AddControllers();
 
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen(config => 
-    config.SwaggerDoc(name:"v1", new OpenApiInfo
+builder.Services.AddSwaggerGen(config =>
+{
+    config.SwaggerDoc(name: "v1", new OpenApiInfo
     {
         Title = "NerdStore Enterprise Identity API",
         Description = "API utilizada para ações de autenticação do sistema",
         Contact = new OpenApiContact { Name = "Paulo Sampaio", Email = "paulohenrique.sales47@gmail.com" },
         License = new OpenApiLicense { Name = "MIT", Url = new Uri("https://opensource.org/licenses/MIT") }
-    }));
+    });
+});
 
 var app = builder.Build();
 

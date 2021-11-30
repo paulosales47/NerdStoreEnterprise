6626c420be9bc6195516bd0c89e9e52bae949137
Paulo Sampaio <paulohenrique.sales47@gmail.com>
2021-11-29 22:50:30 -0300

    Configurando autenticação

---
+++ src/web/mvc/NSE.WebApp.MVC/Configuration/WebAppConfig.cs
@@ -0,0 +1,21 @@
+﻿namespace NSE.WebApp.MVC.Configuration
+{
+    public static class WebAppConfig
+    {
+        public static void UseMvcConfiguration(this WebApplication app) 
+        {
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
+            app.UseRouting();
+            app.UseIdentityConfiguration();
+
+            app.MapControllerRoute(name: "default", pattern: "{controller=Home}/{action=Index}/{id?}");
+        }
+
+        public static void UseMvcConfigurationDevelopment(this IApplicationBuilder app) 
+        {
+            app.UseExceptionHandler("/Home/Error");
+            app.UseHsts();
+        }
+    }
+}
---
+++ src/web/mvc/NSE.WebApp.MVC/Controllers/IdentidadeController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace NSE.WebApp.MVC.Controllers
+{
+    public class IdentidadeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}

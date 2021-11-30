5ad1a216571631c7dff7e6c771aa7ffdfe4939cb
Paulo Sampaio <paulohenrique.sales47@gmail.com>
2021-11-29 23:43:03 -0300

    Models, Views e Controllers de login

--- src/web/mvc/NSE.WebApp.MVC/Controllers/IdentidadeController.cs
+++ src/web/mvc/NSE.WebApp.MVC/Controllers/IdentidadeController.cs
@@ -1,12 +1,47 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using NSE.WebApp.MVC.Models;
 
 namespace NSE.WebApp.MVC.Controllers
 {
     public class IdentidadeController : Controller
     {
-        public IActionResult Index()
+        [HttpGet]
+        [Route("nova-conta")]
+        public IActionResult Registro() { return View(); }
+
+        [HttpPost]
+        [Route("nova-conta")]
+        public async Task<IActionResult> Registro(UsuarioRegistro usuarioRegistro) 
         {
-            return View();
+            if(!ModelState.IsValid) return View(usuarioRegistro);
+
+            //REALIZAR REGISTRO NA API DE IDENTIDADE
+            
+            return RedirectToAction("Index", "Home");
         }
+
+        [HttpGet]
+        [Route("login")]
+        public IActionResult Login() { return View(); }
+
+        [HttpPost]
+        [Route("login")]
+        public async Task<IActionResult> Login(UsuarioLogin usuarioLogin) 
+        {
+            if (!ModelState.IsValid) return View(usuarioLogin);
+
+            //REALIZAR LOGIN NA API DE IDENTIDADE
+
+            return RedirectToAction("Index", "Home");
+        }
+
+        [HttpGet]
+        [Route("sair")]
+        public async Task<IActionResult> Logout() 
+        {
+            return RedirectToAction("Index", "Home");
+        }
+
+
     }
 }

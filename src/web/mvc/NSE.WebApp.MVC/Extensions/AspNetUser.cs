35648ab47f192f8fd452a19bd1e0941b8e3b72f4
Paulo Sampaio <paulohenrique.sales47@gmail.com>
2021-11-30 19:42:23 -0300

    Obtendo dados de usuário do cookie

---
+++ src/web/mvc/NSE.WebApp.MVC/Extensions/AspNetUser.cs
@@ -0,0 +1,53 @@
+﻿using System.Security.Claims;
+
+namespace NSE.WebApp.MVC.Extensions
+{
+    public class AspNetUser: IUser
+    {
+        private readonly IHttpContextAccessor _contextAccessor;
+        private readonly ClaimsPrincipal? _user;
+
+        public AspNetUser(IHttpContextAccessor contextAccessor)
+        {
+            _contextAccessor = contextAccessor;
+            _user = _contextAccessor.HttpContext?.User;
+        }
+
+        public string Name => _user.Identity.Name;
+
+        public bool EstaAutenticado()
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool EstaNaRole(string role)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<Claim> ObterClaims()
+        {
+            throw new NotImplementedException();
+        }
+
+        public HttpContent ObterHttpContext()
+        {
+            throw new NotImplementedException();
+        }
+
+        public string ObterUserEmail()
+        {
+            return EstaAutenticado() ? _user.FindFirstValue(ClaimTypes.Email) : String.Empty;
+        }
+
+        public Guid ObterUserId()
+        {
+            return EstaAutenticado() ? Guid.Parse(_user.FindFirstValue(ClaimTypes.NameIdentifier)) : Guid.Empty;
+        }
+
+        public string ObterUserToken()
+        {
+            return EstaAutenticado() ? _user.FindFirstValue(ClaimTypes.Sid) : String.Empty;
+        }
+    }
+}
---
+++ src/web/mvc/NSE.WebApp.MVC/Extensions/IUser.cs
@@ -0,0 +1,16 @@
+﻿using System.Security.Claims;
+
+namespace NSE.WebApp.MVC.Extensions
+{
+    public interface IUser
+    {
+        string Name { get; }
+        Guid ObterUserId();
+        string ObterUserEmail();
+        string ObterUserToken();
+        bool EstaAutenticado();
+        bool EstaNaRole(string role);
+        IEnumerable<Claim> ObterClaims();
+        HttpContent ObterHttpContext();
+    }
+}

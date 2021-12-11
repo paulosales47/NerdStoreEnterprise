283692554347048d0f38f5687034915b2e5a1e30
Paulo Sampaio <paulohenrique.sales47@gmail.com>
2021-12-11 00:04:41 -0300

    Definindo as entidades na API  de clientes

---
+++ src/services/cliente/NSE.Cliente.API/Models/Cliente.cs
@@ -0,0 +1,24 @@
+﻿using NSE.Core.DomainObjects;
+
+namespace NSE.Cliente.API.Models
+{
+    public class Cliente: Entity, IAgregateRoot
+    {
+        public Cliente(string nome, string email, string cpf)
+        {
+            Nome = nome;
+            Email = email;
+            Cpf = cpf;
+            Excluido = false;
+        }
+
+        //EF
+        protected Cliente() { }
+
+        public string? Nome { get; private set; }
+        public string? Email { get; private set; }
+        public string? Cpf { get; private set; }
+        public bool Excluido { get; private set; }
+        public Endereco? Enderero { get; private set; }
+    }
+}

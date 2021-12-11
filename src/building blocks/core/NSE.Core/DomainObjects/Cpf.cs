3cf7ebd6da02001c6c5235a78cbfa4e11ff12434
Paulo Sampaio <paulohenrique.sales47@gmail.com>
2021-12-11 00:38:50 -0300

    Criando objetos de valor e validações

---
+++ src/building blocks/core/NSE.Core/DomainObjects/Cpf.cs
@@ -0,0 +1,96 @@
+﻿using System.Text;
+
+namespace NSE.Core.DomainObjects
+{
+    public class Cpf
+    {
+        public const int CpfMaxLenght = 11;
+        public string? Numero { get; private set; }
+
+        protected Cpf() { }
+
+        public Cpf(string numero)
+        {
+            if (!ValidateCpf(numero))
+                throw new DomainException("CPF inválido");
+
+            Numero = numero;
+        }
+
+        public static bool ValidateCpf(string cpf)
+        {
+            bool valid = false;
+
+            cpf = ClearAndCheck(cpf, ref valid);
+
+            if (!valid)
+                return false;
+
+            int sum = 0;
+            int remainder = 0;
+
+            for (int i = 0; i < 9; i++)
+                sum += (cpf[i] - 48) * ((8 - i) % 9 + 2);
+
+            remainder = sum % 11;
+
+            if (remainder < 2)
+                remainder = 0;
+            else
+                remainder = 11 - remainder;
+
+            if (cpf[9] != (char)(remainder + 48))
+                return false;
+
+            sum = remainder * 2;
+
+            for (int i = 0; i < 9; i++)
+                sum += (cpf[i] - 48) * ((9 - i) % 10 + 2);
+
+            remainder = sum % 11;
+
+            if (remainder < 2)
+                remainder = 0;
+            else
+                remainder = 11 - remainder;
+
+            if (cpf[10] != (char)(remainder + 48))
+                return false;
+
+            return true;
+        }
+
+        private static string ClearAndCheck(string value, ref bool valid)
+        {
+            if (string.IsNullOrWhiteSpace(value))
+                return value;
+
+            bool firstChar = true;
+            char currentChar = char.MinValue;
+
+            StringBuilder ret = new StringBuilder();
+
+            foreach (var c in value)
+            {
+                if (c >= '0' && c <= '9')
+                {
+                    ret.Append(c);
+
+                    if (firstChar)
+                    {
+                        firstChar = false;
+                        currentChar = c;
+                    }
+                    else if (!valid && currentChar != c)
+                        valid = true;
+                }
+            }
+
+            if (ret.Length != 11)
+                valid = false;
+
+            return ret.ToString();
+        }
+
+    }
+}
---
+++ src/building blocks/core/NSE.Core/DomainObjects/Email.cs
@@ -0,0 +1,26 @@
+﻿using System.Text.RegularExpressions;
+
+namespace NSE.Core.DomainObjects
+{
+    public class Email
+    {
+        protected Email() { }
+
+        public Email(string endereco)
+        {
+            if (!ValidateEmail(endereco))
+                throw new DomainException("E-mail inválido");
+
+            Endereco = endereco;
+        }
+
+        public string? Endereco { get; private set; }
+
+        private static bool ValidateEmail(string email) 
+        {
+            var regexEmail = new Regex(@"^(?!\.)(""([^""\r\\]|\\[""\r\\]) * ""|([-a-z0-9!#$%&'*+/=?^_`{|}~]|(?<!\.)\.)*)(?<!\.)@[a-z0-9][\w\.-]*[a-z0-9]\.[a-z][a-z\.]*[a-z]$");
+            return regexEmail.IsMatch(email);   
+        }
+
+    }
+}
